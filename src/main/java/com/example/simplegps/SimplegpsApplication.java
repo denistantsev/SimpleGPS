5f8bac235da65d54381d899b2c0fc1ba98501066
denistantsev <denistantsev@google.com>
2020-12-19 22:41:37 +0200

    Initial commit

---
+++ src/main/java/com/example/simplegps/SimplegpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplegps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplegpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplegpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplegps/SimplegpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplegps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplegpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
